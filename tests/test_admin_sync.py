01b3069145e4fcc2d089adf99a997958b78014d1
Amin Alaee <mohammadamin.alaee@gmail.com>
2021-12-28 12:29:36 +0100

    Enable Github Actions

--- tests/test_admin_sync.py
+++ tests/test_admin_sync.py
@@ -7,12 +7,12 @@ from sqlalchemy.orm import Session, relationship, sessionmaker
 from starlette.applications import Starlette
 from starlette.testclient import TestClient
 
-from sqladmin.application import Admin
-from sqladmin.models import ModelAdmin
+from sqladmin import Admin, ModelAdmin
+from tests.common import TEST_DATABASE_URI
 
 Base = declarative_base()  # type: Any
 
-engine = create_engine("sqlite:///tmp.db", connect_args={"check_same_thread": False})
+engine = create_engine(TEST_DATABASE_URI, connect_args={"check_same_thread": False})
 
 LocalSession = sessionmaker(bind=engine)
 
