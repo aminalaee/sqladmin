57e5a22b0a9eb290fca46261e33d1d90ee3d5b14
yuxf <yuxf@unitechs.com>
2022-01-28 11:08:49 +0800

    add logout button.

---
+++ sqladmin/statics/js/logout.js
@@ -0,0 +1,12 @@
+//设置cookie
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function logout() {
+    setCookie("access_token", "", -1);
+    self.location = "/admin/login";
+}
