e950c4211b6721b00d00d3aeb421e5b5463ff362
Amin Alaee <mohammadamin.alaee@gmail.com>
2022-07-28 15:06:48 +0200

    Add tests for SQLModel relationships

--- tests/test_integrations/test_sqlmodel.py
+++ tests/test_integrations/test_sqlmodel.py
@@ -1,10 +1,10 @@
-from typing import Any, AsyncGenerator, Optional
+from typing import Any, AsyncGenerator, Optional, List
 from uuid import UUID, uuid4
 
 import pytest
 from httpx import AsyncClient
 from sqlalchemy.orm import sessionmaker
-from sqlmodel import Field, Session, SQLModel
+from sqlmodel import Field, Session, SQLModel, Relationship
 
 from sqladmin.forms import get_model_form
 from tests.common import sync_engine as engine
@@ -16,12 +16,23 @@ LocalSession = sessionmaker(bind=engine, class_=Session)
 session: Session = LocalSession()
 
 
+class Team(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    headquarters: str
+
+    heroes: List["Hero"] = Relationship()
+
+
 class Hero(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     uuid: UUID = Field(default_factory=uuid4)
     name: str = Field(index=True, max_length=5)
     secret_name: str
     age: Optional[int] = None
+
+    team_id: Optional[int] = Field(default=None, foreign_key="team.id")
+    team: Optional[Team] = Relationship()
 
 
 @pytest.fixture
@@ -37,5 +48,13 @@ async def client(prepare_database: Any) -> AsyncGenerator[AsyncClient, None]:
         yield c
 
 
-async def test_model_form_converter_exception(client: AsyncClient) -> None:
-    await get_model_form(model=Hero, engine=engine)
+async def test_model_form_converter(client: AsyncClient) -> None:
+    hero_form = await get_model_form(model=Hero, engine=engine)
+
+    assert "age" in hero_form()._fields
+    assert "team" in hero_form()._fields
+
+    team_form = await get_model_form(model=Team, engine=engine)
+
+    assert "headquarters" in team_form()._fields
+    assert "heroes" in team_form()._fields
