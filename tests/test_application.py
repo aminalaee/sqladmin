58b08bd3e7c1ff7f48f5f9b6a6d4ffe28e6e80d8
Amin Alaee <mohammadamin.alaee@gmail.com>
2022-08-03 10:15:21 +0200

    Fix middlewares not being applied (#267)

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,23 +1,14 @@
-from typing import Any
-
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, sessionmaker
 from starlette.applications import Starlette
+from starlette.datastructures import MutableHeaders
 from starlette.testclient import TestClient
+from starlette.types import ASGIApp, Message, Receive, Scope, Send
 
 from sqladmin import Admin
 from tests.common import sync_engine as engine
 
-Base = declarative_base()  # type: Any
-
-LocalSession = sessionmaker(bind=engine)
-
-session: Session = LocalSession()
-
-app = Starlette()
-
 
 def test_application_title() -> None:
+    app = Starlette()
     Admin(app=app, engine=engine)
 
     with TestClient(app) as client:
@@ -29,6 +20,7 @@ def test_application_title() -> None:
 
 
 def test_application_logo() -> None:
+    app = Starlette()
     Admin(
         app=app,
         engine=engine,
@@ -44,3 +36,31 @@ def test_application_logo() -> None:
         '<img src="https://example.com/logo.svg" width="64" height="64"'
         in response.text
     )
+
+
+def test_middlewares() -> None:
+    class CorrelationIdMiddleware:
+        def __init__(self, app: ASGIApp) -> None:
+            self.app = app
+
+        async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+            async def send_wrapper(message: Message) -> None:
+                if message["type"] == "http.response.start":
+                    headers = MutableHeaders(scope=message)
+                    headers.append("X-Correlation-ID", "UUID")
+                await send(message)
+
+            await self.app(scope, receive, send_wrapper)
+
+    app = Starlette()
+    Admin(
+        app=app,
+        engine=engine,
+        middlewares=[CorrelationIdMiddleware],
+    )
+
+    with TestClient(app) as client:
+        response = client.get("/admin")
+
+    assert response.status_code == 200
+    assert "x-correlation-id" in response.headers
