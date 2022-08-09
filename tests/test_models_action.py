f1420b0e06543c7708317724b356c6454e07d880
just-an-dev <just-an-dev@protonmail.com>
2022-08-09 13:39:23 +0200

    add test of custom endpoint

--- tests/test_models_action.py
+++ tests/test_models_action.py
@@ -1,11 +1,13 @@
 from typing import Any, Generator
 
 import pytest
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from starlette.applications import Starlette
 from starlette.requests import Request
+from starlette.responses import JSONResponse
+from starlette.testclient import TestClient
 
 from sqladmin import Admin, ModelView
 from sqladmin.application import action
@@ -23,7 +25,14 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+
+
+class UserAdmin(ModelView, model=User):
+    @action(name="Trigger", add_in_detail=True, add_in_list=True)
+    async def action_on_user(self, request: Request):
+        model = await self.get_model_by_pk(request.path_params["pk"])
+
+        return JSONResponse({"user_id": model.id})
 
 
 @pytest.fixture(autouse=True)
@@ -33,25 +42,27 @@ def prepare_database() -> Generator[None, None, None]:
     Base.metadata.drop_all(engine)
 
 
-def test_model_action() -> None:
-    class UserAdmin(ModelView, model=User):
-        @action(name="Trigger Update", add_in_detail=True, add_in_list=True)
-        async def action_on_user(self, request: Request):
-            model = await self.get_model_by_pk(request.path_params["pk"])
+@pytest.fixture
+def client() -> Generator[TestClient, None, None]:
+    with TestClient(app=app, base_url="http://testserver") as c:
+        yield c
 
-            return model.id
 
-    admin = Admin(app=Starlette(), engine=engine)
+def test_model_action(client: TestClient) -> None:
     admin.add_view(UserAdmin)
 
-    assert UserAdmin.custom_actions_in_list == ["Trigger Update"]
-    assert UserAdmin.custom_actions_in_detail == ["Trigger Update"]
+    assert UserAdmin.custom_actions_in_list == ["Trigger"]
+    assert UserAdmin.custom_actions_in_detail == ["Trigger"]
 
     with Session() as session:
         user = User()
         session.add(user)
         session.commit()
 
-        url = UserAdmin()._url_for_action(user, "Trigger Update")
+        url = UserAdmin()._url_for_action(user, "Trigger")
 
-    assert url == "/admin/user/action/1/Trigger Update"
+    assert url == "/admin/user/action/1/Trigger"
+
+    response = client.get("/admin/user/action/1/Trigger")
+    assert response.status_code == 200
+    assert response.json() == {"user_id": 1}
