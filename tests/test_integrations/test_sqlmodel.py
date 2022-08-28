a7df8eb7631e90c0a4514be354683d8b5ba68137
Amin Alaee <mohammadamin.alaee@gmail.com>
2022-08-28 10:13:17 +0200

    Add SQLModel relationship tests (#290)

--- tests/test_integrations/test_sqlmodel.py
+++ tests/test_integrations/test_sqlmodel.py
@@ -1,10 +1,10 @@
-from typing import Any, AsyncGenerator, Optional
+from typing import AsyncGenerator, List, Optional
 from uuid import UUID, uuid4
 
 import pytest
 from httpx import AsyncClient
 from sqlalchemy.orm import sessionmaker
-from sqlmodel import Field, Session, SQLModel
+from sqlmodel import Field, Relationship, Session, SQLModel
 
 from sqladmin.forms import get_model_form
 from tests.common import sync_engine as engine
@@ -16,6 +16,14 @@ LocalSession = sessionmaker(bind=engine, class_=Session)
 session: Session = LocalSession()
 
 
+class Team(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    headquarters: str
+
+    heroes: List["Hero"] = Relationship(back_populates="team")
+
+
 class Hero(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     uuid: UUID = Field(default_factory=uuid4)
@@ -23,19 +31,24 @@ class Hero(SQLModel, table=True):
     secret_name: str
     age: Optional[int] = None
 
+    team_id: Optional[int] = Field(default=None, foreign_key="team.id")
+    team: Optional[Team] = Relationship(back_populates="heroes")
 
-@pytest.fixture
+
+@pytest.fixture(autouse=True)
 def prepare_database() -> AsyncGenerator[None, None]:
     SQLModel.metadata.create_all(engine)
     yield
     SQLModel.metadata.drop_all(engine)
 
 
-@pytest.fixture
-async def client(prepare_database: Any) -> AsyncGenerator[AsyncClient, None]:
-    async with AsyncClient(base_url="http://testserver") as c:
-        yield c
+async def test_model_form_converter() -> None:
+    hero_form = await get_model_form(model=Hero, engine=engine)
 
+    assert "age" in hero_form()._fields
+    assert "team" in hero_form()._fields
 
-async def test_model_form_converter_exception(client: AsyncClient) -> None:
-    await get_model_form(model=Hero, engine=engine)
+    team_form = await get_model_form(model=Team, engine=engine)
+
+    assert "headquarters" in team_form()._fields
+    assert "heroes" in team_form()._fields
