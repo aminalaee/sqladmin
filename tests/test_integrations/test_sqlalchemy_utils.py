de633e8f45773aa6103e9904023008cc27cd7c6a
Amin Alaee <mohammadamin.alaee@gmail.com>
2023-02-03 10:51:30 +0000

    Add sqlalchemy_utils ArrowType tests

--- tests/test_integrations/test_sqlalchemy_utils.py
+++ tests/test_integrations/test_sqlalchemy_utils.py
@@ -2,6 +2,7 @@ import pytest
 from sqlalchemy import Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_utils import (
+    ArrowType,
     ColorType,
     CurrencyType,
     EmailType,
@@ -25,6 +26,7 @@ async def test_model_form_sqlalchemy_utils() -> None:
         __tablename__ = "sqlalchemy_utils_model"
 
         id = Column(Integer, primary_key=True)
+        arrow = Column(ArrowType)
         email = Column(EmailType)
         ip = Column(IPAddressType)
         uuid = Column(UUIDType)
@@ -35,6 +37,18 @@ async def test_model_form_sqlalchemy_utils() -> None:
         color = Column(ColorType)
 
     Form = await get_model_form(model=SQLAlchemyUtilsModel, engine=engine)
-    data = DummyData(currency="IR", timezone=["Iran/Tehran"], color="bbb", phone="abc")
+    data = DummyData(
+        currency="IR", timezone=["Iran/Tehran"], color="bbb", phone="abc", arrow="wrong"
+    )
     form = Form(data)
     assert form.validate() is False
+
+    data = DummyData(
+        currency="IRR",
+        timezone="Asia/Tehran",
+        color="red",
+        phone="+9823456789",
+        arrow="2023-02-06 12:00:0",
+    )
+    form = Form(data)
+    assert form.validate() is True
