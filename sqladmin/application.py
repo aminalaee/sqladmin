e2837dd27e94025d9ea92b161afccb506e47986e
Amin Alaee <mohammadamin.alaee@gmail.com>
2023-07-24 14:08:30 +0200

    Fix expose and action Auth backend not called

--- sqladmin/application.py
+++ sqladmin/application.py
@@ -94,7 +94,6 @@ class BaseAdmin:
 
         self.admin = Starlette(middleware=middlewares)
         self._views: List[Union[BaseView, ModelView]] = []
-
         self.templates = self.init_templating_engine()
 
     def init_templating_engine(self) -> Jinja2Templates:
@@ -133,6 +132,8 @@ class BaseAdmin:
         """Add ModelView or BaseView classes to Admin.
         This is a shortcut that will handle both `add_model_view` and `add_base_view`.
         """
+
+        view._admin_ref = self
         if view.is_model:
             self.add_model_view(view)  # type: ignore
         else:
