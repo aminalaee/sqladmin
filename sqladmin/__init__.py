04b7b480cd29106c6f362513d1803040ad6c64a6
Amin Alaee <mohammadamin.alaee@gmail.com>
2023-10-12 20:47:12 +0200

    Pin wtforms version below 3.1.0 (#646)
    * uprev 0.15.2

--- sqladmin/__init__.py
+++ sqladmin/__init__.py
@@ -1,7 +1,7 @@
 from sqladmin.application import Admin, action, expose
 from sqladmin.models import BaseView, ModelView
 
-__version__ = "0.15.1"
+__version__ = "0.15.2"
 
 __all__ = [
     "Admin",
