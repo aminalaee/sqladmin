52df929287d5899953261c051f30630ecb217e48
Nicolás Carvajal <66692533+ncarvajalc@users.noreply.github.com>
2023-11-13 04:36:37 -0500

    Fix multiple PK model containing boolean values (#670)
    Co-authored-by: Amin Alaee <mohammadamin.alaee@gmail.com>

--- sqladmin/helpers.py
+++ sqladmin/helpers.py
@@ -225,7 +225,9 @@ def object_identifier_values(id_string: str, model: Any) -> tuple:
     values = []
     pks = get_primary_keys(model)
     for pk, part in zip(pks, _object_identifier_parts(id_string, model)):
-        values.append(get_column_python_type(pk)(part))
+        type_ = get_column_python_type(pk)
+        value = False if type_ is bool and part == "False" else type_(part)
+        values.append(value)
     return tuple(values)
 
 
