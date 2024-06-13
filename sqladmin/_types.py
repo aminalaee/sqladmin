7a5277b32c555a069301e65744257514a49b2c54
Amin Alaee <mohammadamin.alaee@gmail.com>
2024-06-13 16:49:15 +0200

    update

--- sqladmin/_types.py
+++ sqladmin/_types.py
@@ -1,9 +1,9 @@
-from __future__ import annotations
+from typing import Union
 
 from sqlalchemy.engine import Engine
 from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlalchemy.orm import ColumnProperty, InstrumentedAttribute, RelationshipProperty
 
-MODEL_PROPERTY = ColumnProperty | RelationshipProperty
-ENGINE_TYPE = Engine | AsyncEngine
-MODEL_ATTR = str | InstrumentedAttribute
+MODEL_PROPERTY = Union[ColumnProperty, RelationshipProperty]
+ENGINE_TYPE = Union[Engine, AsyncEngine]
+MODEL_ATTR = Union[str, InstrumentedAttribute]
