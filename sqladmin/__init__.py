ad873160d3bf6ab322e2629bc4cfcbeb24103c37
Amin Alaee <mohammadamin.alaee@gmail.com>
2024-07-01 12:49:31 +0200

    Update __init__.py

--- sqladmin/__init__.py
+++ sqladmin/__init__.py
@@ -1,7 +1,7 @@
 from sqladmin.application import Admin, action, expose
 from sqladmin.models import BaseView, ModelView
 
-__version__ = "0.17.0"
+__version__ = "0.18.0"
 
 __all__ = [
     "Admin",
