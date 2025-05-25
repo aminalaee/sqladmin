98f62bd8964e1be342c862de3dac37689f4055a2
Robin Billy <robin@probabl.ai>
2025-05-25 22:54:02 +0200

    fix: CategoryMenu is_active logic
    The `CategoryMenu.is_active` function was incorrectly returning `True` if any of the children were visible.

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -42,6 +42,32 @@ def test_category_menu():
     assert item_menu.type_ == "Category"
 
 
+def test_category_menu_is_active_when_child_is_active():
+    request = Request(
+        {
+            "type": "http",
+            "path_params": {"identity": "user"},
+        }
+    )
+    user_menu = ViewMenu(view=UserAdmin(), name="user")
+
+    category_menu = CategoryMenu(name="Models")
+    category_menu.add_child(user_menu)
+
+    assert user_menu.is_active(request) is True
+    assert category_menu.is_active(request) is True
+
+
+def test_category_menu_is_not_active_when_no_child_is_active():
+    user_menu = ViewMenu(view=UserAdmin(), name="user")
+
+    category_menu = CategoryMenu(name="Models")
+    category_menu.add_child(user_menu)
+
+    assert user_menu.is_active(request) is False
+    assert category_menu.is_active(request) is False
+
+
 def test_view_menu():
     item_menu = ViewMenu(view=UserAdmin(), name="view")
 
