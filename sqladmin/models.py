d99efd97be0c437c98272cbb6679d08700f6a98a
M. Krostelev <mikhai.krostelev@x5.ru>
2025-08-21 22:46:23 +0300

    fix: linters

--- sqladmin/models.py
+++ sqladmin/models.py
@@ -683,7 +683,7 @@ class ModelView(BaseView, metaclass=ModelViewMeta):
         ]
         self._relation_names = [relation.key for relation in self._mapper.relationships]
 
-        self._non_link_fields: Union[List[str],None] = None
+        self._non_link_fields: Union[List[str], None] = None
 
         self._column_labels = self._build_column_pairs(self.column_labels)
         self._column_labels_value_by_key = {
