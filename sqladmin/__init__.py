921e592d0d11a1b443670d150182c40b595962ef
Amin Alaee <mohammadamin.alaee@gmail.com>
2025-11-24 13:36:03 +0100

    Version 0.22.0

--- sqladmin/__init__.py
+++ sqladmin/__init__.py
@@ -1,7 +1,7 @@
 from sqladmin.application import Admin, action, expose
 from sqladmin.models import BaseView, ModelView
 
-__version__ = "0.21.0"
+__version__ = "0.22.0"
 
 __all__ = [
     "Admin",
