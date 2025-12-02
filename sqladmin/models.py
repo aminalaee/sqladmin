4f9360eb7847992da1742ca07fd9c413adade962
Emil Styrke <emil.styrke@gmail.com>
2025-12-02 09:05:54 +0100

    Add select_from to count query in models.py
    This prevents crashes in multi-bind scenarios.

--- sqladmin/models.py
+++ sqladmin/models.py
@@ -1199,7 +1199,7 @@ class ModelView(BaseView, metaclass=ModelViewMeta):
         By default it will select all objects without any filters.
         """
 
-        return select(func.count(self.pk_columns[0]))
+        return select(func.count(self.pk_columns[0])).select_from(self.model)
 
     def sort_query(self, stmt: Select, request: Request) -> Select:
         """
